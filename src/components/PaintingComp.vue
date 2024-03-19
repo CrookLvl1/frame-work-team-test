8aefee4b3c47879d6d15f8ec9a48cdc10a6fd73f
Konstantin <kostzis693@gmail.com>
2024-03-19 16:16:42 +0500

    PaintingComp - completed

--- src/components/PaintingComp.vue
+++ src/components/PaintingComp.vue
@@ -1,21 +1,37 @@
 <template>
-  <v-img :src="url" cover class="painting w-100 h-100">
+  <v-img :src="url" cover class="painting w-100 h-100 painting-responsive-img">
     <template v-slot:placeholder>
       <v-sheet class="d-flex h-100 align-center justify-center">
         <v-progress-circular color="primary" indeterminate></v-progress-circular>
       </v-sheet>
     </template>
 
-    <v-sheet class="info w-100 px-4">
-      <span class="text-static"> 12312412401240 12941924912941 9249124 </span>
-    </v-sheet>
+    <div class="info w-100">
+      <p class="static-text">{{ painting.name }}</p>
+      <ul class="desc">
+        <li class="desc-text">
+          <span>Author: </span>
+          <span>{{ author }}</span>
+        </li>
+        <li class="desc-text">
+          <span>Created: </span>
+          <span>{{ painting.created }}</span>
+        </li>
+        <li class="desc-text">
+          <span>Location: </span>
+          <span>{{ location }}</span>
+        </li>
+      </ul>
+    </div>
   </v-img>
 </template>
 
 <script setup lang="ts">
 import { Painting } from '@/ts/Types';
-import { PropType } from 'vue';
+import { PropType, computed } from 'vue';
 import baseURL from '@/ts/baseURL';
+import { useStore } from '@/store';
+const store = useStore();
 
 const props = defineProps({
   painting: {
@@ -24,45 +40,66 @@ const props = defineProps({
   },
 });
 
+const author = computed(
+  () => store.getAuthors.find((author) => author.id === props.painting.authorId)?.name || 'undefined',
+);
+const location = computed(
+  () => store.getLocations.find((location) => location.id === props.painting.locationId)?.location || 'undefined',
+);
+
 const url = baseURL.slice(0, -1) + props.painting.imageUrl;
-console.log(url);
 </script>
 
 <style langs="scss" scoped>
-.v-img {
-  aspect-ratio: 360/275;
-  @media (max-width: 768.75px) {
-    aspect-ratio: 340/249;
-  }
-}
 .painting {
-  border-radius: 20px;
   cursor: pointer;
 
+  border-radius: 20px;
+
   .info {
-    background-color: rgba(255, 255, 255, 0.8);
     position: absolute;
     bottom: 0;
-    /* transform: translateY(80%); */
-    height: 30px;
-    transition: height 250ms ease-out;
 
-    span {
-      display: block;
-      max-width: 100%;
+    height: calc(145 / 275 * 100%);
+    padding: 0 15px;
+
+    transition: transform 250ms ease-out;
+    transform: translateY(80%);
+
+    background-color: rgba(255, 255, 255, 0.8);
+
+    p {
       overflow: hidden;
+
+      box-sizing: border-box;
+      max-width: 100%;
+      padding: 5px 0 6px;
+
       white-space: nowrap;
       text-overflow: ellipsis;
-      /* box-sizing: border-box; */
+    }
+
+    .desc {
+      display: flex;
+      flex-direction: column;
+
+      list-style: none;
+
+      gap: 6px 0;
+
+      > li span:first-child {
+        font-weight: 700;
+      }
     }
   }
 
   &:hover {
     .info {
-      /* transform: translateY(0%); */
-      height: calc(145 / 275 * 100%);
+      overflow-y: auto;
 
-      span {
+      transform: translateY(0%);
+
+      li {
         text-wrap: wrap;
       }
     }
